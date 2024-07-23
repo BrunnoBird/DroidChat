38ca8c6f1d721eb8967ef8bbd1e2875cc287ad80
brunnobird <bird.tec.gonzalez@gmail.com>
2024-07-23 20:59:50 -0300

    feat: initialize

---
+++ app/src/main/java/com/bgabird/droidchat/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.bgabird.droidchat
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bgabird.droidchat.navigation.ChatNavHost
+import com.bgabird.droidchat.ui.theme.DroidChatTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DroidChatTheme {
+                ChatNavHost()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DroidChatTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/bgabird/droidchat/navigation/ChatNavHost.kt
@@ -0,0 +1,34 @@
+package com.bgabird.droidchat.navigation
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+
+const val SPLASH_ROUTE = "splash"
+const val SIGN_IN_ROUTE = "signIn"
+const val SIGN_UP_ROUTE = "signUp"
+
+@Composable
+fun ChatNavHost() {
+
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = SPLASH_ROUTE) {
+        composable(SPLASH_ROUTE) {
+            Text(
+                text = "Hello World",
+            )
+        }
+
+        composable(SIGN_IN_ROUTE) {
+
+        }
+
+        composable(SIGN_UP_ROUTE) {
+
+        }
+    }
+}
