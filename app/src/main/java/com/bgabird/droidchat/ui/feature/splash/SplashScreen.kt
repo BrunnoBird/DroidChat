726b680f57a5e6af205222a81231dd0438dd2246
brunnobird <bird.tec.gonzalez@gmail.com>
2024-07-24 21:20:56 -0300

    feat: atualiza splash screen

--- app/src/main/java/com/bgabird/droidchat/ui/feature/splash/SplashScreen.kt
+++ app/src/main/java/com/bgabird/droidchat/ui/feature/splash/SplashScreen.kt
@@ -13,26 +13,33 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bgabird.droidchat.R
 import com.bgabird.droidchat.ui.theme.BackgroundGradient
 import com.bgabird.droidchat.ui.theme.DroidChatTheme
+import kotlinx.coroutines.delay
 
 @Composable
-fun SplashRoute() {
+fun SplashRoute(
+    onNavigateToSignIn: () -> Unit,
+) {
     SplashScreen()
+    LaunchedEffect(Unit) {
+        delay(2000)
+        onNavigateToSignIn()
+    }
 }
 
 @Composable
 fun SplashScreen() {
-    val context = LocalContext.current
 
     Column(
         modifier = Modifier
@@ -61,7 +68,7 @@ fun SplashScreen() {
             Spacer(modifier = Modifier.width(8.dp))
 
             Text(
-                text = context.getString(R.string.splash_safety_info),
+                text = stringResource(id = R.string.splash_safety_info),
                 color = Color.White,
                 textAlign = TextAlign.Center,
                 style = MaterialTheme.typography.bodyMedium
