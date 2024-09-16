6c97d5277fc780e87c27723c5129ecce169e9caa
brunnobird <bird.tec.gonzalez@gmail.com>
2024-09-16 08:23:21 -0300

    feat: start primary button component

---
+++ app/src/main/java/com/bgabird/droidchat/ui/component/PrimaryButton.kt
@@ -0,0 +1,19 @@
+package com.bgabird.droidchat.ui.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bgabird.droidchat.ui.theme.DroidChatTheme
+
+@Composable
+fun PrimaryButton(modifier: Modifier = Modifier) {
+    
+}
+
+@Preview
+@Composable
+private fun PrimaryButtonPreview() {
+    DroidChatTheme {
+        PrimaryButton()
+    }
+}
