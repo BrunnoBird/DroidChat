112e72a31a26cedf23a40aacdc3009aaeaf879c6
brunnobird <bird.tec.gonzalez@gmail.com>
2024-09-23 07:32:28 -0300

    feat: implement button PrimaryButton

--- app/src/main/java/com/bgabird/droidchat/ui/component/PrimaryButton.kt
+++ app/src/main/java/com/bgabird/droidchat/ui/component/PrimaryButton.kt
@@ -1,19 +1,87 @@
 package com.bgabird.droidchat.ui.component
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bgabird.droidchat.ui.theme.DroidChatTheme
 
+
 @Composable
-fun PrimaryButton(modifier: Modifier = Modifier) {
-    
+fun PrimaryButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    isLoading: Boolean = false,
+) {
+
+    Button(
+        onClick = {
+            onClick()
+        },
+        modifier = modifier
+            .height(64.dp),
+        enabled = !isLoading,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary,
+            disabledContainerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.8f)
+        )
+    ) {
+        Box(
+            modifier = modifier.animateContentSize()
+        ) {
+            if (isLoading) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .size(30.dp)
+                        .aspectRatio(1f),
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    strokeCap = StrokeCap.Round
+                )
+            } else {
+                Text(
+                    text = text,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
 }
 
 @Preview
 @Composable
 private fun PrimaryButtonPreview() {
     DroidChatTheme {
-        PrimaryButton()
+        PrimaryButton(
+            text = "Sign In",
+            onClick = {},
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PrimaryButtonLoadingPreview() {
+    DroidChatTheme {
+        PrimaryButton(
+            text = "Sign In",
+            onClick = {},
+            isLoading = true
+        )
     }
 }
--- app/src/main/java/com/bgabird/droidchat/ui/feature/signIn/SignInScreen.kt
+++ app/src/main/java/com/bgabird/droidchat/ui/feature/signIn/SignInScreen.kt
@@ -7,7 +7,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,11 +18,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bgabird.droidchat.R
+import com.bgabird.droidchat.ui.component.PrimaryButton
 import com.bgabird.droidchat.ui.component.PrimaryChatTextField
 import com.bgabird.droidchat.ui.theme.BackgroundGradient
 import com.bgabird.droidchat.ui.theme.DroidChatTheme
@@ -34,7 +40,9 @@ fun SignInScreen() {
     Column(
         Modifier
             .fillMaxSize()
-            .background(brush = BackgroundGradient),
+            .verticalScroll(rememberScrollState())
+            .background(brush = BackgroundGradient)
+            .imePadding(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
@@ -47,9 +55,10 @@ fun SignInScreen() {
 
 
         Column(
-            modifier = Modifier.padding(horizontal = 16.dp)
+            modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.spacing_medium))
         ) {
             var email by remember { mutableStateOf("") }
+
             PrimaryChatTextField(
                 value = email,
                 onInputChange = {
@@ -62,6 +71,7 @@ fun SignInScreen() {
             Spacer(modifier = Modifier.height(16.dp))
 
             var password by remember { mutableStateOf("") }
+
             PrimaryChatTextField(
                 value = password,
                 onInputChange = {
@@ -70,6 +80,17 @@ fun SignInScreen() {
                 placeholder = "Digite sua senha",
                 leftIcon = R.drawable.ic_lock,
                 keyboardType = KeyboardType.Password
+            )
+
+            Spacer(modifier = Modifier.height(98.dp))
+
+            var isLoading by remember { mutableStateOf(false) }
+
+            PrimaryButton(text = stringResource(id = R.string.feature_login_button),
+                onClick = {
+                    isLoading = !isLoading
+                },
+                isLoading = isLoading
             )
         }
     }
