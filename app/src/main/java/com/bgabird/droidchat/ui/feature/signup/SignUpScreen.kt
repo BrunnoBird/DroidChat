cb88eeeacb5531da113c4a65e19ca801cddd0bb5
brunnobird <bird.tec.gonzalez@gmail.com>
2024-10-12 13:08:31 -0300

    feat: add pick a picture in gallery

--- app/src/main/java/com/bgabird/droidchat/ui/feature/signup/SignUpScreen.kt
+++ app/src/main/java/com/bgabird/droidchat/ui/feature/signup/SignUpScreen.kt
@@ -3,6 +3,7 @@ package com.bgabird.droidchat.ui.feature.signup
 import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -14,12 +15,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -31,16 +35,19 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bgabird.droidchat.R
 import com.bgabird.droidchat.ui.component.PrimaryButton
+import com.bgabird.droidchat.ui.component.ProfilePictureOptionsModalBottomSheet
 import com.bgabird.droidchat.ui.component.ProfilePictureSelection
 import com.bgabird.droidchat.ui.component.SecondaryTextField
 import com.bgabird.droidchat.ui.theme.BackgroundGradient
 import com.bgabird.droidchat.ui.theme.DroidChatTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun SignUpRoute() {
     SignUpScreen()
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun SignUpScreen() {
     Box(
@@ -58,6 +65,10 @@ private fun SignUpScreen() {
 
             var profilePictureSelectedUri by remember {
                 mutableStateOf<Uri?>(null)
+            }
+
+            var openProfilePictureOptionsModalBottomSheet by remember {
+                mutableStateOf(false)
             }
 
             Image(
@@ -82,7 +93,11 @@ private fun SignUpScreen() {
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     ProfilePictureSelection(
-                        imageUri = profilePictureSelectedUri
+                        imageUri = profilePictureSelectedUri,
+                        modifier = Modifier
+                            .clickable {
+                                openProfilePictureOptionsModalBottomSheet = true
+                            }
                     )
 
                     Spacer(modifier = Modifier.height(30.dp))
@@ -142,6 +157,27 @@ private fun SignUpScreen() {
 
                     Spacer(modifier = Modifier.height(16.dp))
                 }
+            }
+
+            val sheetState = rememberModalBottomSheetState()
+            val scope = rememberCoroutineScope()
+            if (openProfilePictureOptionsModalBottomSheet) {
+                ProfilePictureOptionsModalBottomSheet(
+                    onPictureSelected = { uri ->
+                        profilePictureSelectedUri = uri
+                        scope.launch {
+                            sheetState.hide()
+                        }.invokeOnCompletion {
+                            if (!sheetState.isVisible) {
+                                openProfilePictureOptionsModalBottomSheet = false
+                            }
+                        }
+                    },
+                    onDismissRequest = {
+                        openProfilePictureOptionsModalBottomSheet = false
+                    },
+                    sheetState = sheetState
+                )
             }
         }
     }
